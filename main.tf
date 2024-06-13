61cb4b91c4ddbd7cb5c84e88f1acf6cf2a1aaa53
Sandesh Thalanki <sandeshthalanki.manjunath@telecomitalia.it>
2024-06-13 05:40:32 +0200

    chore: Update GCP bucket name in main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ## the bucketname is prefixed with the GCP project name which is unique
 resource "google_storage_bucket" "auto-expire" {
-  name          = "tf-gcp-demo-377217-bucket"   
+  name          = "sandesh-bucket"   
   location      = "US"
   force_destroy = true
 
