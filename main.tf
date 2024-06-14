f355592dc2905a2c4894b871c91b4f83555f723a
sandesh <sandeshthalanki.manjunath@telecomitalia.it>
2024-06-14 14:01:51 +0200

    Update bucket name in main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ## the bucketname is prefixed with the GCP project name which is unique
 resource "google_storage_bucket" "auto-expire" {
-  name          = "bucket-1406-1342"
+  name          = "bucket-1406-1401"
   location      = "US"
   force_destroy = true
 
