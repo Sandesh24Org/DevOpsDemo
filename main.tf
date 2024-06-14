1958391e5d456d12ec400cd886650f6abdede182
sandesh <sandeshthalanki.manjunath@telecomitalia.it>
2024-06-14 15:30:27 +0200

    Update bucket and add GKE cluster and node pool

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ## the bucketname is prefixed with the GCP project name which is unique
 resource "google_storage_bucket" "auto-expire" {
-  name          = "bucket-1406-1401"
+  name          = "bucket-tf-cloud-1406-1523"
   location      = "US"
   force_destroy = true
 
@@ -20,5 +20,41 @@ resource "google_storage_bucket" "auto-expire" {
     action {
       type = "AbortIncompleteMultipartUpload"
     }
+  }
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "gke-tf-cloud-1406-1523"
+  location = "us-central1"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-micro"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 }
