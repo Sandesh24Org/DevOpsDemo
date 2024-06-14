36b6548894180a47cb01122599de6af3017768c5
sandesh <sandeshthalanki.manjunath@telecomitalia.it>
2024-06-14 16:14:29 +0200

    Update bucket and cluster type

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
-## the bucketname is prefixed with the GCP project name which is unique
 resource "google_storage_bucket" "auto-expire" {
-  name          = "bucket-tf-cloud-1406-1553"
+  name          = "bucket-tf-cloud-1406-1612"
   location      = "US"
   force_destroy = true
 
@@ -24,38 +23,8 @@ resource "google_storage_bucket" "auto-expire" {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "gke-tf-cloud-1406-1553"
+  name     = "gke-tf-cloud-1406-1612"
   location = "us-central1"
 
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  master_auth {
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-}
-
-resource "google_container_node_pool" "primary" {
-  name       = "my-node-pool"
-  location   = "us-central1"
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-micro"
-    disk_size_gb = 50
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
+  enable_autopilot = true
 }
