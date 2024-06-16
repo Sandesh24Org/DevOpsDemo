51218aef1b514e6799f0fce5150769467183c9ff
sandesh <sandeshthalanki.manjunath@telecomitalia.it>
2024-06-16 09:06:49 +0200

    Refactor lifecycle rules in main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "bucket-tf-cloud-1406-1612"
-  location      = "US"
-  force_destroy = true
+# resource "google_storage_bucket" "auto-expire" {
+#   name          = "bucket-tf-cloud-1406-1612"
+#   location      = "US"
+#   force_destroy = true
 
-  lifecycle_rule {
-    condition {
-      age = 3
-    }
-    action {
-      type = "Delete"
-    }
-  }
+#   lifecycle_rule {
+#     condition {
+#       age = 3
+#     }
+#     action {
+#       type = "Delete"
+#     }
+#   }
 
-  lifecycle_rule {
-    condition {
-      age = 1
-    }
-    action {
-      type = "AbortIncompleteMultipartUpload"
-    }
-  }
-}
+#   lifecycle_rule {
+#     condition {
+#       age = 1
+#     }
+#     action {
+#       type = "AbortIncompleteMultipartUpload"
+#     }
+#   }
+# }
 
 # resource "google_container_cluster" "primary" {
 #   name     = "gke-tf-cloud-1406-1612"
